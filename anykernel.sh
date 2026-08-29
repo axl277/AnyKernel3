7ce5be4092b2606ec0cf4368ca67bf0cbbf66591
Axlorin° <154013417+axl277@users.noreply.github.com>
2026-08-29 23:27:39 +0700

    Fix capitalization in kernel string

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Custom Kernel For POCO X3 GT / Redmi Note 10 pro 5G
+kernel.string=Custom Kernel For POCO X3 GT / Redmi Note 10 Pro 5G
 do.devicecheck=0
 do.modules=0
 do.systemless=1
